6fa270edb8aeb46063ac8042a102cb3c0f0e825c
Alexander <roux@yandex.ru>
2023-06-12 21:07:56 +0200

    Add classes example in main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+#include <jtag.h>
+
+JtagWire tck = JtagWire(2, OUTPUT);
+JtagWire tms = JtagWire(3, OUTPUT);
+JtagWire tdi = JtagWire(4, OUTPUT);
+JtagWire tdo = JtagWire(5, INPUT);
+JtagWire trst = JtagWire(6, OUTPUT);
+
+JtagBus jtag_bus = JtagBus();
+Jtag arm_jtag = Jtag();
 
 void setup() {
   // put your setup code here, to run once:
+  // pinMode(LED_BUILTIN, OUTPUT);
+  jtag_bus.assign_pin(JTAGPin::TCK, tck);
+  jtag_bus.assign_pin(JTAGPin::TMS, tms);
+  jtag_bus.assign_pin(JTAGPin::TDI, tdi);
+  jtag_bus.assign_pin(JTAGPin::TDO, tdo);
+  jtag_bus.assign_pin(JTAGPin::TRST, trst);
+
+  arm_jtag.add_bus(jtag_bus);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  // delay(1000);                      // wait for a second
+  // digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+  // delay(1000);
+
+  // tdi.set();
+  // delay(1000);
+  // tdi.clear();
+  // tdi.pulse_high(1000000UL);
+
+  jtag_bus.clock(1, 1);
+  delay(1000);
+  jtag_bus.clock(0, 0);
+  delay(1000);
 }
