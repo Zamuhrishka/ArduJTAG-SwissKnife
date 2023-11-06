d93d6a991cd1395bfa31b970659baaa40ac16768
Alexander <roux@yandex.ru>
2023-11-06 18:16:26 +0100

    Apply clang-format

--- include/jtag.h
+++ include/jtag.h
@@ -9,38 +9,41 @@
 
 //_____ I N C L U D E S _______________________________________________________
 #include <Arduino.h>
+#include <CircularBuffer.h>
 //_____ C O N F I G S  ________________________________________________________
 //_____ D E F I N I T I O N S _________________________________________________
 namespace JTAG
 {
-    const uint8_t PINS_NUMBER = 5;
+  const uint8_t PINS_NUMBER = 5;
 
-    enum class CONSTANTS : uint32_t {
-        MAX_SPEED_KHZ = 500,
-        MAX_SEQUENCE_LEN = 256,
-        MAX_SEQUENCE_LEN_BYTES = MAX_SEQUENCE_LEN / 8, // 32
-    };
+  enum class CONSTANTS : uint32_t
+  {
+    MAX_SPEED_KHZ = 500,
+    MAX_SEQUENCE_LEN = 256,
+    MAX_SEQUENCE_LEN_BYTES = MAX_SEQUENCE_LEN / 8,  // 32
+  };
 
-    enum class PIN : uint8_t
-    {
-        TCK = 0,
-        TMS = 1,
-        TDI = 2,
-        TDO = 3,
-        TRST = 4,
-    };
+  enum class PIN : uint8_t
+  {
+    TCK = 0,
+    TMS = 1,
+    TDI = 2,
+    TDO = 3,
+    TRST = 4,
+  };
 
-    enum class ERROR : int32_t {
-        NO = 0,
-        INVALID_PIN = -1,
-        INVALID_SPEED = -2,
-        INVALID_SEQUENCE_LEN = -3,
-    };
+  enum class ERROR : int32_t
+  {
+    NO = 0,
+    INVALID_PIN = -1,
+    INVALID_SPEED = -2,
+    INVALID_SEQUENCE_LEN = -3,
+  };
 
-    void setBitArray(int i_bit, byte *data, int value);
-    void setBitArray(uint32_t i_bit, char *data, char value);
-    int getBitArray(int i_bit, const byte *data);
-    char getBitArray(uint32_t i_bit, const char *data);
+  void setBitArray(int i_bit, byte *data, int value);
+  void setBitArray(uint32_t i_bit, char *data, char value);
+  int getBitArray(int i_bit, const byte *data);
+  char getBitArray(uint32_t i_bit, const char *data);
 }
 //_____ C L A S S E S __________________________________________________________
 /**
@@ -49,22 +52,23 @@ namespace JTAG
  */
 class JtagPin
 {
-    public:
-        JtagPin() = delete;
-        explicit JtagPin(int pin, int dir);
+public:
+  JtagPin() = delete;
+  explicit JtagPin(int pin, int dir);
 
-        void setHigh();
-        void setLow();
-        int get() const;
-        void pulseHigh(size_t us);
-        void pulseLow(size_t us);
-        void setValue(int value);
-        void assign(int pin, int dir);
-        void setDir(int dir);
-        int getDir() const;
-    private:
-        uint32_t pin = 0;
-        uint8_t dir = INPUT;
+  void setHigh();
+  void setLow();
+  int get() const;
+  void pulseHigh(size_t us);
+  void pulseLow(size_t us);
+  void setValue(int value);
+  void assign(int pin, int dir);
+  void setDir(int dir);
+  int getDir() const;
+
+private:
+  uint32_t pin = 0;
+  uint8_t dir = INPUT;
 };
 
 /**
@@ -73,23 +77,29 @@ class JtagPin
  */
 class JtagBus
 {
-    public:
-        JtagBus() = delete;
-        explicit JtagBus(JtagPin tms, JtagPin tdi, JtagPin tdo, JtagPin tck, JtagPin rst);
+public:
+  JtagBus() = delete;
+  explicit JtagBus(JtagPin tms, JtagPin tdi, JtagPin tdo, JtagPin tck, JtagPin rst);
 
-        JTAG::ERROR setSpeed(uint32_t khz);
-        uint32_t getSpeed() const;
-        void reset();
-        uint8_t clock(uint8_t tms, uint8_t tdi);
-        JTAG::ERROR sequence(size_t n, const byte tms[], const byte tdi[], byte *tdo);
-    private:
-        uint32_t last_tck_micros = 0;
-        uint32_t min_tck_micros = 1;
-        JtagPin _tms;
-        JtagPin _tdi;
-        JtagPin _tdo;
-        JtagPin _tck;
-        JtagPin _rst;
+  JTAG::ERROR setSpeed(uint32_t khz);
+  uint32_t getSpeed() const;
+  void reset();
+  uint8_t clock(uint8_t tms, uint8_t tdi);
+  JTAG::ERROR sequence(size_t n, const byte tms[], const byte tdi[], byte *tdo);
+
+private:
+  uint32_t last_tck_micros = 0;
+  uint32_t min_tck_micros = 1;
+  JtagPin _tms;
+  JtagPin _tdi;
+  JtagPin _tdo;
+  JtagPin _tck;
+  JtagPin _rst;
+
+  uint8_t tms_buf[64];
+  uint8_t tdi_buf[64];
+  uint8_t tdo_buf[64];
+  uint32_t offset = 0;
 };
 
 /**
@@ -98,19 +108,20 @@ class JtagBus
  */
 class Jtag
 {
-    public:
-        Jtag() = delete;
-        explicit Jtag(uint8_t tms, uint8_t tdi, uint8_t tdo, uint8_t tck, uint8_t trst);
+public:
+  Jtag() = delete;
+  explicit Jtag(uint8_t tms, uint8_t tdi, uint8_t tdo, uint8_t tck, uint8_t trst);
 
-        void ir(uint32_t length, byte* command, byte* output);
-        void ir(const char *command, byte* output);
-        void ir(const char *command, char* output);
-        void dr(uint32_t length, byte* data, byte* output);
-        void dr(const char *data, byte* output);
-        void dr(const char *data, char* output);
-        void reset();
-        JTAG::ERROR setSpeed(uint32_t khz);
-        uint8_t clock(uint8_t tms, uint8_t tdi);
-    private:
-        JtagBus bus;
+  void ir(uint32_t length, byte *command, byte *output);
+  void ir(const char *command, byte *output);
+  void ir(const char *command, char *output);
+  void dr(uint32_t length, byte *data, byte *output);
+  void dr(const char *data, byte *output);
+  void dr(const char *data, char *output);
+  void reset();
+  JTAG::ERROR setSpeed(uint32_t khz);
+  uint8_t clock(uint8_t tms, uint8_t tdi);
+
+private:
+  JtagBus bus;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,11 @@
 
 #include "jtag.h"
 
-
 #define TCK 2
 #define TMS 3
 #define TDI 4
 #define TDO 5
 #define RST 6
-
 
 // Create CLI Object
 SimpleCLI cli;
@@ -27,19 +25,326 @@ const uint8_t IR_FULL_LEN = 16;
 const uint8_t DR_LEN = 33;
 const uint8_t DR_FULL_LEN = 38;
 
-char output[256] = {};
+// char output[256] = {};
+char output[500] = {};
 
-void helpCallback(cmd* c) {
+static void jtag_idcode()
+{
+  Serial.println("Read of `IDCODE` `DP`register");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("011111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000000000", output);
+  Serial.print("> ");
+  Serial.println(output);
+  delay(300);
+}
+
+static void jtag_idcode2()
+{
+  Serial.println("Read of `IDCODE` `DP`register");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("111110000", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000000000", output);
+  Serial.print("> ");
+  Serial.println(output);
+  delay(300);
+}
+
+char d_sample[] =
+  "00000000000001101101101100110110110000000000000000000000110110110110110110110110110110110000000000000000"
+  "00000011011011011011011011011011000000000000000000000011011011011011011011011011011011011000000000000000"
+  "00000001101101101101101101101101101101100000000000000000000001101101101101101101101101101100000000000001"
+  "1011011011011011011011011000000000000000000000000000000000000000000011011011000011011011011011";
+
+static void jtag_sample()
+{
+  Serial.println("SAMPLE");
+  arm_jtag.reset();
+  // memset(output, '\0', sizeof(output));
+  arm_jtag.ir("111101000", output);
+  // memset(output, '\0', sizeof(output));
+  arm_jtag.dr(d_sample, output);
+  // Serial.print("> ");
+  // Serial.println(output);
+  delay(300);
+}
+
+static void jtag_perload()
+{
+  Serial.println("PERLOAD");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("111101000", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr(
+    "000000000000011011011011001101101100000000000000000000001101101101101101101101101101101100000000000000000000001101"
+    "101101101101101101101100000000000000000000001001001001001101101101101101101101100000000000000000000001101101101101"
+    "101101101101101101100000000000000000000001101101101101101101101101101100000000000001101101101101101101101101100000"
+    "0000000000000000000000000000000000000011011011000011011011011011",
+    output);
+  Serial.print("> ");
+  Serial.println(output);
+  delay(300);
+}
+
+static void jtag_extest()
+{
+  Serial.println("EXTEST");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("111100000", output);
+  // memset(output, '\0', sizeof(output));
+  // arm_jtag.dr(
+  //   "000000000000011011011011001101101100000000000000000000001101101101101101101101101101101100000000000000000000001101"
+  //   "101101101101101101101100000000000000000000001001001001001101101101101101101101100000000000000000000001101101101101"
+  //   "101101101101101101100000000000000000000001101101101101101101101101101100000000000001101101101101101101101101100000"
+  //   "0000000000000000000000000000000000000011011011000011011011011011",
+  //   output);
+  // Serial.print("> ");
+  // Serial.println(output);
+  delay(300);
+}
+
+static void jtag_enable_pwr()
+{
+  Serial.println("Enable power and clock on DAP");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("010000000000000000000000000000010100", output);
+}
+
+static void jtag_csw_rd()
+{
+  Serial.print("CSW: ");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("001000000000000000000000000000000000", output);
+
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("100000000000000000000000000000000000", output);
+
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000000000000", output);
+  Serial.println(output);
+}
+
+static void jtag_base_rd()
+{
+  Serial.print("BASE: ");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("001000011110000000000000000000000000", output);
+
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("101000000000000000000000000000000000", output);
+
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000000000000", output);
+  Serial.println(output);
+}
+
+static void jtag_abort_wr()
+{
+  Serial.print("ABORT: ");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000111110000000000000000000000000000", output);
+}
+
+static void jtag_csw_wr()
+{
+  Serial.print("CSW: ");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("001000000000000000000000000000000000", output);
+
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000010001000", output);
+}
+
+static void jtag_ctrl_read()
+{
+  Serial.print("CTRL/STAT: ");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("110000000000000000000000000000000000", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000000000000", output);
+  Serial.println(output);
+}
+
+static String reverseString(const String &input)
+{
+  String reversed = "";
+  for (int i = input.length() - 1; i >= 0; i--)
+  {
+    reversed += input.charAt(i);
+  }
+  return reversed;
+}
+
+String toBinaryString(uint32_t num)
+{
+  String binary = "";
+  for (int i = 0; i < 32; i++)
+  {
+    binary += (num & 0x80000000UL) ? '1' : '0';
+    num <<= 1;
+  }
+  return binary;
+}
+
+static void jtag_mem_write(uint32_t addr, uint32_t data)
+{
+  String address = toBinaryString(addr);
+  String rev_addr = reverseString(address);
+  String tar = "010" + rev_addr + "0";
+
+  String str_data = toBinaryString(data);
+  String rev_data = reverseString(str_data);
+  String drw = "011" + rev_data + "0";
+
+  Serial.print("Write of addr: ");
+  Serial.print(addr, HEX);
+  Serial.print(" -> ");
+  Serial.println(tar);
+
+  Serial.println("Step 1: Config AP access");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("001000000000000000000000000000000000", output);
+
+  Serial.println("Step 2: Configure the CSW register");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000010000000000000000000000000001000", output);
+
+  Serial.println("Step 3: Configure the TAR register");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr(tar.c_str(), output);
+
+  // delay(100);
+
+  Serial.println("Step 4: Write memory");
+  Serial.print("\t-->: ");
+  Serial.println(str_data);
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr(drw.c_str(), output);
+
+  Serial.print("\t");
+  jtag_ctrl_read();
+  Serial.print("\t");
+  jtag_csw_rd();
+}
+
+static void jtag_mem_read(uint32_t addr)
+{
+  String address = toBinaryString(addr);
+  String rev_addr = reverseString(address);
+  String tar = "010" + rev_addr + "0";
+
+  Serial.print("Read of addr: ");
+  Serial.print(addr, HEX);
+  Serial.print(" -> ");
+  Serial.println(tar);
+
+  Serial.println("Step 1: Config AP access");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("010111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("001000000000000000000000000000000000", output);
+
+  Serial.println("Step 2: Configure the CSW register");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000010000000000000000000000000001000", output);
+
+  Serial.println("Step 3: Configure the TAR register");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr(tar.c_str(), output);
+
+  delay(100);
+
+  Serial.println("Step 4: Read memory");
+  Serial.print("\t<--: ");
+  arm_jtag.reset();
+  memset(output, '\0', sizeof(output));
+  arm_jtag.ir("110111111", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("111000000000000000000000000000000000", output);
+  memset(output, '\0', sizeof(output));
+  arm_jtag.dr("000000000000000000000000000000000000", output);
+  Serial.println(output);
+
+  Serial.print("\t");
+  jtag_ctrl_read();
+  Serial.print("\t");
+  jtag_csw_rd();
+}
+
+static void blink(void)
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
+
+void helpCallback(cmd *c)
+{
   Serial.println("This is Help command");
 }
 
-void versionCallback(cmd* c) {
+void versionCallback(cmd *c)
+{
   Serial.println("ver.0.0.1b");
 }
 
 // Callback function for ping command
-void irCallback(cmd* c) {
-  Command cmd(c); // Create wrapper object
+void irCallback(cmd *c)
+{
+  Command cmd(c);  // Create wrapper object
 
   // Get arguments
   Argument ir_code = cmd.getArgument("code");
@@ -57,8 +362,9 @@ void irCallback(cmd* c) {
 }
 
 // Callback function for ping command
-void drCallback(cmd* c) {
-  Command cmd(c); // Create wrapper object
+void drCallback(cmd *c)
+{
+  Command cmd(c);  // Create wrapper object
 
   // Get arguments
   Argument dr_data = cmd.getArgument("data");
@@ -75,8 +381,9 @@ void drCallback(cmd* c) {
   memset(output, 0, sizeof(output));
 }
 
-void clockCallback(cmd* c) {
-  Command cmd(c); // Create wrapper object
+void clockCallback(cmd *c)
+{
+  Command cmd(c);  // Create wrapper object
 
   // Get arguments
   Argument arg_tms = cmd.getArgument("tms");
@@ -93,34 +400,35 @@ void clockCallback(cmd* c) {
   Serial.println(tdo);
 }
 
-void resetCallback(cmd* c) {
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(500);                      // wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(500);                      // wait for a second
+void resetCallback(cmd *c)
+{
+  blink();
 
   arm_jtag.reset();
   Serial.println("> Reset");
 }
 
 // Callback in case of an error
-void errorCallback(cmd_error* e) {
-  CommandError cmdError(e); // Create wrapper object
+void errorCallback(cmd_error *e)
+{
+  CommandError cmdError(e);  // Create wrapper object
 
   Serial.print("ERROR: ");
   Serial.println(cmdError.toString());
 
-  if (cmdError.hasCommand()) {
-      Serial.print("Did you mean \"");
-      Serial.print(cmdError.getCommand().toString());
-      Serial.println("\"?");
+  if (cmdError.hasCommand())
+  {
+    Serial.print("Did you mean \"");
+    Serial.print(cmdError.getCommand().toString());
+    Serial.println("\"?");
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-  cli.setOnError(errorCallback); // Set error Callback
+  cli.setOnError(errorCallback);  // Set error Callback
 
   // Create the ir command with callback function
   ir = cli.addCommand("ir", irCallback);
@@ -140,9 +448,11 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // Check if user typed something into the serial monitor
-  if (Serial.available()) {
+  if (Serial.available())
+  {
     // Read out string from the serial monitor
     String input = Serial.readStringUntil('\n');
 
@@ -150,16 +460,62 @@ void loop() {
     cli.parse(input);
   }
 
-  if (cli.errored()) {
+  if (cli.errored())
+  {
     CommandError cmdError = cli.getError();
 
     Serial.print("ERROR: ");
     Serial.println(cmdError.toString());
 
-    if (cmdError.hasCommand()) {
+    if (cmdError.hasCommand())
+    {
       Serial.print("Did you mean \"");
       Serial.print(cmdError.getCommand().toString());
       Serial.println("\"?");
     }
   }
+
+  delay(500);
+
+  jtag_perload();
+  jtag_extest();
+
+  // jtag_enable_pwr();
+  // jtag_ctrl_read();
+  // jtag_idcode();
+  // jtag_idcode2();
+  // jtag_sample();
+  // jtag_csw_wr();
+  // jtag_csw_rd();
+
+  // jtag_mem_write(0x20000000, 0xFF00FFFF);
+  // jtag_mem_read(0x20000000);
+
+  // jtag_mem_read(0xE00FF000);
+
+  // jtag_base_rd();
+  // jtag_ctrl_read();
+  // jtag_csw_rd();
+
+  // jtag_mem_read(0xA0000000);
+  // jtag_mem_read(0x60000000);
+  // jtag_mem_read(0x50050000);
+  // jtag_mem_read(0x40026400);
+  // jtag_mem_read(0x40013800);
+  // jtag_mem_read(0x40005000);
+
+  // jtag_mem_read(0xE0000000);  // 00010100011010000000000000000000000000000
+
+  // jtag_mem_write(0xE002EDF0, 0xA05F0003);
+  // jtag_mem_read(0xE002EDF0);
+  // jtag_mem_read(0xE000EDF0);
+
+  // jtag_mem_read(0xE0042000);
+  // jtag_mem_read(0xE00FF000);
+  // jtag_mem_read(0xE00FFFF0);
+  // jtag_mem_read(0xE00FFFF4);
+  // jtag_mem_read(0xE00FFFF8);
+  // jtag_mem_read(0xE00FFFFC);
+
+  // jtag_abort_wr();
 }
