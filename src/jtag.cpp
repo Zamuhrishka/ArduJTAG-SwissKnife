fb4e773202b63080719d4ad1dc7bf727ee0e4db1
Alexander <roux@yandex.ru>
2024-01-03 17:51:13 +0100

    Remove JTAG_PRIV namespace

--- src/jtag.cpp
+++ src/jtag.cpp
@@ -13,27 +13,20 @@
 #include "JtagCommon.hpp"
 
 #include <assert.h>
-
 //_____ C O N F I G S  ________________________________________________________
-#define DEBUG_MODE
-
 //_____ D E F I N I T I O N S _________________________________________________
-namespace JTAG_PRIV
-{
-  const uint8_t RESET_TMS = 0x1F;
-  const uint8_t RESET_TMS_LEN = 5;
+const uint8_t RESET_TMS = 0x1F;
+const uint8_t RESET_TMS_LEN = 5;
 
-  const uint8_t IR_TMS_PRE = 6;
-  const uint8_t IR_TMS_POST = 1;
-  const uint8_t IR_TMS_PRE_LEN = 5;
-  const uint8_t IR_TMS_POST_LEN = 2;
+const uint8_t IR_TMS_PRE = 6;
+const uint8_t IR_TMS_POST = 1;
+const uint8_t IR_TMS_PRE_LEN = 5;
+const uint8_t IR_TMS_POST_LEN = 2;
 
-  const uint8_t DR_TMS_PRE = 1;
-  const uint8_t DR_TMS_POST = 1;
-  const uint8_t DR_TMS_PRE_LEN = 3;
-  const uint8_t DR_TMS_POST_LEN = 2;
-}  // namespace JTAG_PRIV
-
+const uint8_t DR_TMS_PRE = 1;
+const uint8_t DR_TMS_POST = 1;
+const uint8_t DR_TMS_PRE_LEN = 3;
+const uint8_t DR_TMS_POST_LEN = 2;
 //_____ C L A S S E S __________________________________________________________
 Jtag::Jtag(uint8_t tms, uint8_t tdi, uint8_t tdo, uint8_t tck, uint8_t rst):
     bus(JtagPin(tms, OUTPUT), JtagPin(tdi, OUTPUT), JtagPin(tdo, INPUT), JtagPin(tck, OUTPUT), JtagPin(rst, OUTPUT))
@@ -44,13 +37,13 @@ void Jtag::ir(uint16_t instruction, uint16_t length)
 {
   assert(length != 0);
 
-  uint8_t tms_pre[1] = {JTAG_PRIV::IR_TMS_PRE};
-  uint8_t tms_post[1] = {JTAG_PRIV::IR_TMS_POST};
+  uint8_t tms_pre[1] = {IR_TMS_PRE};
+  uint8_t tms_post[1] = {IR_TMS_POST};
   uint8_t tdi_pre[1] = {0x00};
   uint8_t tdi_post[1] = {0x00};
 
   // Sending PreBitwise
-  for (uint16_t i_seq = 0; i_seq < JTAG_PRIV::IR_TMS_PRE_LEN; i_seq++)
+  for (uint16_t i_seq = 0; i_seq < IR_TMS_PRE_LEN; i_seq++)
   {
     this->bus.clock(JTAG::getBitArray(i_seq, &tms_pre[0]), JTAG::getBitArray(i_seq, &tdi_pre[0]));
   }
@@ -63,7 +56,7 @@ void Jtag::ir(uint16_t instruction, uint16_t length)
   this->bus.clock(1, JTAG::getBitArray(length - 1, (uint8_t *)&instruction));
 
   // Sending PostBitwise
-  for (uint16_t i_seq = 0; i_seq < JTAG_PRIV::IR_TMS_POST_LEN; i_seq++)
+  for (uint16_t i_seq = 0; i_seq < IR_TMS_POST_LEN; i_seq++)
   {
     this->bus.clock(JTAG::getBitArray(i_seq, &tms_post[0]), JTAG::getBitArray(i_seq, &tdi_post[0]));
   }
@@ -74,13 +67,13 @@ void Jtag::dr(uint8_t *data, uint32_t length, uint8_t *output)
   assert(data != nullptr);
   assert(length != 0);
 
-  uint8_t tms_pre[1] = {JTAG_PRIV::DR_TMS_PRE};
-  uint8_t tms_post[1] = {JTAG_PRIV::DR_TMS_POST};
+  uint8_t tms_pre[1] = {DR_TMS_PRE};
+  uint8_t tms_post[1] = {DR_TMS_POST};
   uint8_t tdi_pre[1] = {0x00};
   uint8_t tdi_post[1] = {0x00};
   uint16_t bit_offset = 0;
 
-  for (uint16_t i_seq = 0; i_seq < JTAG_PRIV::DR_TMS_PRE_LEN; i_seq++)
+  for (uint16_t i_seq = 0; i_seq < DR_TMS_PRE_LEN; i_seq++)
   {
     JTAG::setBitArray(bit_offset,
                       &output[0],
@@ -98,7 +91,7 @@ void Jtag::dr(uint8_t *data, uint32_t length, uint8_t *output)
 
   bit_offset++;
 
-  for (uint16_t i_seq = 0; i_seq < JTAG_PRIV::DR_TMS_POST_LEN; i_seq++)
+  for (uint16_t i_seq = 0; i_seq < DR_TMS_POST_LEN; i_seq++)
   {
     this->bus.clock(JTAG::getBitArray(i_seq, &tms_post[0]), JTAG::getBitArray(i_seq, &tdi_post[0]));
   }
@@ -111,11 +104,11 @@ JTAG::ERROR Jtag::sequence(size_t n, const uint8_t tms[], const uint8_t tdi[], u
 
 void Jtag::reset()
 {
-  uint8_t tms = JTAG_PRIV::RESET_TMS;
+  uint8_t tms = RESET_TMS;
   uint8_t tdi = 0x00;
   uint8_t tdo = 0x00;
 
-  this->bus.sequence(JTAG_PRIV::RESET_TMS_LEN, &tms, &tdi, &tdo);
+  this->bus.sequence(RESET_TMS_LEN, &tms, &tdi, &tdo);
 }
 
 JTAG::ERROR Jtag::setSpeed(uint32_t khz)
